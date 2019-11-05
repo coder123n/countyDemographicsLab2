62c38b94ab4f13076568267935d1af0c366ed761
coder123n <naomisellis@gmail.com>
2019-11-05 09:58:58 -0800

    lwts see

--- countyDemographics.py
+++ countyDemographics.py
@@ -5,18 +5,26 @@ import json
 app = Flask(__name__) 
 
 def get_state_options(counties):
-    """Compute and return an interesting fact using the demographic data about the counties in the US."""
     listofstates=[]
+    option=""
     for data in counties:
         if data["State"] not in listofstates:
             listofstates.append(data["State"])
-    return listofstates
+    for data in listofstates:
+        option=option+Markup("<option value=\"" + data + "\">" + data + "</option>")
+    return option
+    
+       
+def get_state_fact(counties):
+    request.args[state]
+    for data in counties:
+        data["Miscellaneous"]["Building Permits"]
 
 @app.route("/")
 def render_main():
     with open('county_demographics.json') as demographics_data:
         counties = json.load(demographics_data)
-    return render_template('home.html',state="get_state_options(counties)")
+    return render_template('home.html',state=get_state_options(counties),fact=get_state_fact(counties))
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)
